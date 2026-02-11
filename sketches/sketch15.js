42f09122c8ca8276760144648361a6c409eb722a
Celine Chen <celinech@uw.edu>
2026-02-10 20:12:36 -0800

    setting up data set table and setup

---
+++ sketches/sketch15.js
@@ -0,0 +1,66 @@
+const types = [
+  "Normal", "Fire", "Water", "Electric", "Grass", "Ice",
+  "Fighting", "Poison", "Ground", "Flying",
+  "Psychic", "Bug", "Rock", "Ghost",
+  "Dragon", "Dark", "Steel", "Fairy"
+];
+
+const typeColors = {
+  Normal: "#A8A77A",
+  Fire: "#EE8130",
+  Water: "#6390F0",
+  Electric: "#F7D02C",
+  Grass: "#7AC74C",
+  Ice: "#96D9D6",
+  Fighting: "#C22E28",
+  Poison: "#A33EA1",
+  Ground: "#E2BF65",
+  Flying: "#A98FF3",
+  Psychic: "#F95587",
+  Bug: "#A6B91A",
+  Rock: "#B6A136",
+  Ghost: "#735797",
+  Dragon: "#6F35FC",
+  Dark: "#705746",
+  Steel: "#B7B7CE",
+  Fairy: "#D685AD"
+};
+
+const dominance = [
+  { from: "Fire", to: "Grass" },
+  { from: "Water", to: "Fire" },
+  { from: "Electric", to: "Water" },
+  { from: "Grass", to: "Water" },
+  { from: "Ice", to: "Dragon" },
+  { from: "Fighting", to: "Dark" },
+  { from: "Poison", to: "Fairy" },
+  { from: "Ground", to: "Electric" },
+  { from: "Psychic", to: "Fighting" },
+  { from: "Bug", to: "Psychic" },
+  { from: "Rock", to: "Flying" },
+  { from: "Ghost", to: "Psychic" },
+  { from: "Dragon", to: "Dragon" },
+  { from: "Dark", to: "Ghost" },
+  { from: "Steel", to: "Ice" },
+  { from: "Fairy", to: "Dragon" }
+];
+
+let centerX, centerY;
+let radius = 280;
+let angleStep;
+let typeAngles = {};
+
+function setup() {
+  createCanvas(900, 900);
+  centerX = width / 2;
+  centerY = height / 2;
+
+  angleStep = TWO_PI / types.length;
+
+  // Assign angle to each type
+  for (let i = 0; i < types.length; i++) {
+    typeAngles[types[i]] = i * angleStep;
+  }
+
+  textFont("Arial");
+}
